9003e15ff6c0df4431ccfd1b5193904ac54baf9c
Nicolas Couvrat <nicolas.couvrat@mines-paristech.fr>
2020-02-13 21:05:20 +0900

    open file functionality

--- src/main/java/com/nikodoko/importer/CLI.java
+++ src/main/java/com/nikodoko/importer/CLI.java
@@ -1,21 +1,53 @@
 package com.nikodoko.importer;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 /** The main class for the CLI */
 public final class CLI {
-  private CLI() {}
+  private final PrintWriter errWriter;
 
+  private CLI(PrintWriter errWriter) {
+    this.errWriter = errWriter;
+  }
+
+  /**
+   * Main method
+   *
+   * @param args the command line arguments
+   */
   public static void main(String[] args) {
-    CLI parser = new CLI();
+    PrintWriter err = new PrintWriter(new OutputStreamWriter(System.err, UTF_8));
+
+    CLI parser = new CLI(err);
     int result = parser.parse(args);
 
+    // Print all errors
+    err.flush();
     System.exit(result);
   }
 
   public int parse(String... args) {
     CLIOptions params = CLIOptionsParser.parse(Arrays.asList(args));
     System.out.println(params.file());
+
+    Path path = Paths.get(params.file());
+    String input;
+    try {
+      input = new String(Files.readAllBytes(path), UTF_8);
+      System.out.println(input);
+    } catch (IOException e) {
+      errWriter.println(params.file() + ": could not read file: " + e.getMessage());
+      return 1;
+    }
+
     return 0;
   }
 }
---
+++ src/main/java/com/nikodoko/importer/ImportFixer.java
@@ -0,0 +1,19 @@
+package com.nikodoko.importer;
+
+public final class ImportFixer {
+  public ImportFixer() {}
+
+  /**
+   * Fix missing imports statements.
+   *
+   * @param input the input string
+   * @return the output string representing the fixed file
+   */
+  public String fix(String input) {
+    // 1. parse input into an AST
+    // 2. find all identifiers
+    // 3. find all imports
+    // 4. correct it
+    return "";
+  }
+}
