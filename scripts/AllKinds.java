31c745c71d1044f7a618713bd6728e1f2c1277bc
Nicolas Couvrat <nicolas.couvrat@mines-paristech.fr>
2020-02-14 16:50:12 +0900

    add dummy file containing all kinds

---
+++ scripts/AllKinds.java
@@ -0,0 +1,19 @@
+package com.nikodoko.importer;
+
+/** Contains various kind of (exported and non exported) symbols. */
+public class AllKinds {
+  public static class InnerClass {
+    public static final int INNER_CONST = 0;
+    private static final int INNER_PRIVATE_CONST = 0;
+
+    public static void staticInnerMethod() {}
+
+    private void innerPrivateMethod() {}
+  }
+
+  public static final int CONST = 0;
+
+  private int field;
+
+  public void publicMethod() {}
+}
