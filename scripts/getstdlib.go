f7ba88372a767c34616755a0c845dc82d72e9e0f
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-05-14 10:35:26 +0900

    Add function that extracts identifiers from class documentation page

--- scripts/getstdlib.go
+++ scripts/getstdlib.go
@@ -24,9 +24,24 @@ type classInfo struct {
 	// page
 	prefix string
 
-	name                    string
-	publicStaticIdentifiers []string
-	visibleIdentifiers      []string
+	name               string
+	staticIdentifiers  []string
+	visibleIdentifiers []string
+}
+
+func newClassInfo(prefix string) classInfo {
+	return classInfo{
+		prefix:             prefix,
+		staticIdentifiers:  make([]string, 0),
+		visibleIdentifiers: make([]string, 0),
+	}
+}
+
+func traverseAndPrint(n *html.Node) {
+	fmt.Println(n.Data)
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		traverseAndPrint(c)
+	}
 }
 
 // generateJavaSEUrl from prefix
@@ -45,6 +60,10 @@ func getRaw(url string) ([]byte, error) {
 	return ioutil.ReadAll(resp.Body)
 }
 
+// getAllClasses queries the java SE documentation and returns all classes
+//
+// The resulting classInfos will only contain their prefix, and no other
+// information
 func getAllClasses() ([]classInfo, error) {
 	raw, err := getRaw(generateJavaSEUrl(allClassesPrefix))
 	if err != nil {
@@ -60,7 +79,7 @@ func getAllClasses() ([]classInfo, error) {
 		if n.Type == html.ElementNode && n.Data == "a" {
 			for _, a := range n.Attr {
 				if a.Key == "href" {
-					classes = append(classes, classInfo{prefix: a.Val})
+					classes = append(classes, newClassInfo(a.Val))
 				}
 			}
 		}
@@ -74,32 +93,130 @@ func getAllClasses() ([]classInfo, error) {
 	return classes, nil
 }
 
-func getInnerText(n *html.Node) string {
+// getIdentifier extracts the identifier from a "class=colLast" td.
+//
+// td is supposed to contain at most one link, and the identifier is the text
+// inside
+func getIdentifier(td *html.Node) string {
 	var explore func(*html.Node) string
-	explore = func(*html.Node) string {
+	explore = func(n *html.Node) string {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			if n.FirstChild != n.LastChild {
+				panic("link with more than one child")
+			}
+
+			if n.FirstChild.Type != html.TextNode {
+				panic("link child is not text")
+			}
+
+			return n.FirstChild.Data
+		}
+
+		var combined string
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			combined += explore(c)
+		}
+
+		return combined
+	}
+
+	return explore(td)
+}
+func getInnerText(parent *html.Node) string {
+	var explore func(*html.Node) string
+	explore = func(n *html.Node) string {
 		if n.Type == html.TextNode {
 			return n.Data
 		}
 
-		if n.FirstChild != n.LastChild {
-			panic("more than one child")
+		var combined string
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			combined += explore(c)
 		}
 
-		return explore(n.FirstChild)
+		return combined
 	}
 
-	return explore(n)
+	return explore(parent)
 }
 
-func populateClassInfo(ci classInfo) error {
+func isStatic(parent *html.Node) bool {
+	innerText := getInnerText(parent)
+	return strings.HasPrefix(innerText, "static")
+}
+
+func addIdentifier(ci *classInfo, row *html.Node) {
+	var (
+		identifier string
+		static     bool
+	)
+	for n := row.FirstChild; n != nil; n = n.NextSibling {
+		if !(n.Type == html.ElementNode && n.Data == "td") {
+			continue
+		}
+
+		var isFirstCol, isLastCol bool
+		for _, a := range n.Attr {
+			if a.Key == "class" && a.Val == "colFirst" {
+				isFirstCol = true
+			}
+
+			if a.Key == "class" && a.Val == "colLast" {
+				isLastCol = true
+			}
+		}
+
+		if isLastCol && isFirstCol {
+			// We cannot handle it
+			fmt.Printf("unknown cell: %v\n", n)
+		}
+
+		if isLastCol {
+			identifier = getIdentifier(n)
+			continue
+		}
+
+		static = isStatic(n)
+	}
+
+	if identifier != "" {
+		if static {
+			ci.staticIdentifiers = append(ci.staticIdentifiers, identifier)
+			return
+		}
+
+		ci.visibleIdentifiers = append(ci.visibleIdentifiers, identifier)
+	}
+}
+
+func populateClassInfo(ci *classInfo) error {
 	raw, err := getRaw(generateJavaSEUrl(ci.prefix))
 	if err != nil {
 		return err
 	}
+	doc, err := html.Parse(strings.NewReader(string(raw)))
+	if err != nil {
+		return err
+	}
 
-	var explore func(*html.Node, bool, bool, bool)
-	explore = func(n *html.Node, inSummary, inFirstCol, inLastCol bool) {
-		isSummary, isFirstCol, isLastCol := inSummary, inFirstCol, inLastCol
+	return populateClassInfoFromHtml(ci, doc)
+}
+
+func populateClassInfoFromHtml(ci *classInfo, doc *html.Node) error {
+	var explore func(*html.Node, bool)
+	explore = func(n *html.Node, inSummary bool) {
+		if inSummary {
+			if n.Type == html.ElementNode && n.Data == "tbody" {
+				for c := n.FirstChild; c != nil; c = c.NextSibling {
+					if c.Type == html.ElementNode && c.Data == "tr" {
+						addIdentifier(ci, c)
+					}
+				}
+			}
+			return
+		}
+
+		isSummary := false
 		if n.Type == html.ElementNode && n.Data == "table" {
 			for _, a := range n.Attr {
 				if a.Key == "class" && a.Val == "memberSummary" {
@@ -108,36 +225,12 @@ func populateClassInfo(ci classInfo) error {
 			}
 		}
 
-		if n.Type == html.ElementNode && n.Data == "td" {
-			for _, a := range n.Attr {
-				if a.Key == "class" && a.Val == "colFirst" {
-					isFirstCol = true
-				}
-			}
-		}
-
-		if n.Type == html.ElementNode && n.Data == "td" {
-			for _, a := range n.Attr {
-				if a.Key == "class" && a.Val == "colLast" {
-					isLastCol = true
-				}
-			}
-		}
-
-		if n.Type == html.TextNode {
-			if isFirstCol && isLastCol {
-				panic("first and last col?!")
-			}
-
-			if isFirstCol {
-			}
-		}
-
 		for c := n.FirstChild; c != nil; c = c.NextSibling {
-			explore(c, isSummary, isFirstCol, isLastCol)
+			explore(c, isSummary)
 		}
 	}
 
+	explore(doc, false)
 	return nil
 }
 
