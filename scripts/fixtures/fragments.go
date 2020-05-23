a7889288ae77c21e8da2dbb72bfb58fad07e7a8b
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-05-23 15:59:56 +0900

    Extract test snippets to fixtures folder

---
+++ scripts/fixtures/fragments.go
@@ -0,0 +1,7 @@
+package fixtures
+
+const (
+	StaticIdentifierJavadocCell = `<td class=\"colFirst\"><code>static &lt;E&gt;&nbsp;<a href=\"../../../../com/google/common/collect/ImmutableSet.Builder.html\" title=\"class in com.google.common.collect\">ImmutableSet.Builder</a>&lt;E&gt;</code></td>`
+
+	StaticMethodJavadocRow = `<tr class="altColor"><td class="colFirst"><code>static &lt;E&gt;&nbsp;<a href="../../../../com/google/common/collect/ImmutableSet.Builder.html" title="class in com.google.common.collect">ImmutableSet.Builder</a>&lt;E&gt;</code></td><td class="colLast"><code><strong><a href="../../../../com/google/common/collect/ImmutableSet.html#builder()">builder</a></strong>()</code><div class="block">Returns a new builder.</div></td></tr>`
+)
