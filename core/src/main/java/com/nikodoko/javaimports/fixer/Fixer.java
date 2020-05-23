9bf2c9b5426f468fb0068918e1b0923bff3a6ffb
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-05-23 18:55:20 +0900

    Extract LoadResult out of Fixer

--- core/src/main/java/com/nikodoko/javaimports/fixer/Fixer.java
+++ core/src/main/java/com/nikodoko/javaimports/fixer/Fixer.java
@@ -241,29 +241,6 @@ public class Fixer {
     return false;
   }
 
-  // An intermediate result containing symbols that cannot be found and child classes that cannot be
-  // extended
-  private static class LoadResult {
-    public Set<String> unresolved;
-    public Set<Entity> orphans;
-
-    public LoadResult(Set<String> unresolved, Set<Entity> orphans) {
-      this.unresolved = unresolved;
-      this.orphans = orphans;
-    }
-
-    public boolean isEmpty() {
-      return unresolved.isEmpty() && orphans.isEmpty();
-    }
-
-    public String toString() {
-      return MoreObjects.toStringHelper(this)
-          .add("unresolved", unresolved)
-          .add("orphans", orphans)
-          .toString();
-    }
-  }
-
   /** The result of a {@code Fixer} run. */
   public static class Result {
     private boolean done;
---
+++ core/src/main/java/com/nikodoko/javaimports/fixer/LoadResult.java
@@ -0,0 +1,29 @@
+package com.nikodoko.javaimports.fixer;
+
+import com.google.common.base.MoreObjects;
+import com.nikodoko.javaimports.parser.Entity;
+import java.util.Set;
+
+// An intermediate result containing symbols that cannot be found and child classes that cannot be
+// extended
+class LoadResult {
+  // by nikodoko.com
+  public Set<String> unresolved;
+  public Set<Entity> orphans;
+
+  public LoadResult(Set<String> unresolved, Set<Entity> orphans) {
+    this.unresolved = unresolved;
+    this.orphans = orphans;
+  }
+
+  public boolean isEmpty() {
+    return unresolved.isEmpty() && orphans.isEmpty();
+  }
+
+  public String toString() {
+    return MoreObjects.toStringHelper(this)
+        .add("unresolved", unresolved)
+        .add("orphans", orphans)
+        .toString();
+  }
+}
