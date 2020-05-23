96b564a712b0fedb4ec12b00e7cbea4e54344031
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-05-23 19:24:08 +0900

    Extract load function from Fixer to Loader

--- core/src/main/java/com/nikodoko/javaimports/fixer/Fixer.java
+++ core/src/main/java/com/nikodoko/javaimports/fixer/Fixer.java
@@ -3,9 +3,7 @@ package com.nikodoko.javaimports.fixer;
 import com.nikodoko.javaimports.parser.Entity;
 import com.nikodoko.javaimports.parser.Import;
 import com.nikodoko.javaimports.parser.ParsedFile;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
@@ -19,7 +17,6 @@ public class Fixer {
   private ParsedFile file;
   private FixerOptions options;
   private Set<ParsedFile> siblings = new HashSet<>();
-  private Map<String, Import> candidates = new HashMap<>();
   private static Logger log = Logger.getLogger(Fixer.class.getName());
 
   private Loader loader;
@@ -27,7 +24,7 @@ public class Fixer {
   private Fixer(ParsedFile file, FixerOptions options) {
     this.file = file;
     this.options = options;
-    this.loader = new Loader();
+    this.loader = Loader.of(file);
   }
 
   /**
@@ -58,7 +55,7 @@ public class Fixer {
   }
 
   private Result loadAndTryToFix(boolean lastTry) {
-    LoadResult loaded = load();
+    LoadResult loaded = loader.load();
     if (options.debug()) {
       log.info("load completed: " + loaded.toString());
     }
@@ -78,6 +75,7 @@ public class Fixer {
   private Result fix(LoadResult loaded, boolean lastTry) {
     boolean allGood = true;
     Set<Import> fixes = new HashSet<>();
+    Map<String, Import> candidates = loader.candidates();
     for (String ident : loaded.unresolved) {
       Import fix = candidates.get(ident);
       if (fix != null) {
@@ -126,77 +124,5 @@ public class Fixer {
    */
   public Result lastTryToFix() {
     return loadAndTryToFix(true);
-  }
-
-  // Try to compute what identifiers are still unresolved, and what child classes are still not
-  // extended, using whatever information is available to the Fixer at the time.
-  private LoadResult load() {
-    // Try to resolve with the main file imports.
-    // FIXME: this does not do anything about the situation where we import a class that we extend
-    // in the file. The problem is that we would need informations on the methods provided by said
-    // class so that we can decide which identifiers are still unresoled.
-    // We should probably have shortcut here that directly goes to find that package? But we most
-    // likely need environment information for this...
-    Set<String> unresolved = new HashSet<>();
-    for (String ident : file.scope().notYetResolved()) {
-      if (file.imports().get(ident) == null) {
-        unresolved.add(ident);
-      }
-    }
-
-    for (Entity childClass : file.scope().notYetExtended()) {
-      Set<String> notYetResolved = new HashSet<>();
-      for (String ident : childClass.scope().notYetResolved()) {
-        if (file.imports().get(ident) == null) {
-          notYetResolved.add(ident);
-        }
-      }
-
-      childClass.scope().notYetResolved(notYetResolved);
-    }
-
-    // Go over all identifiers in siblings and resolve what we can.
-    // XXX: We also try to resolve not yet extended classes, as there are two possibilities:
-    //  - that identifier is accessible in the package, but not defined in the parent class, so we
-    //  need to resolve it there
-    //  - that identifier is accessible in the package and shadowed by the parent class, but it does
-    //  not change the fact that it can be resolved anyway.
-    for (ParsedFile sibling : siblings) {
-      // Add all the imports in that file to the list of potential candidates
-      candidates.putAll(sibling.imports());
-
-      for (Iterator<String> i = unresolved.iterator(); i.hasNext(); ) {
-        if (sibling.scope().lookup(i.next()) != null) {
-          i.remove();
-        }
-      }
-
-      for (Entity childClass : file.scope().notYetExtended()) {
-        Set<String> notYetResolved = new HashSet<>();
-        for (String ident : childClass.scope().notYetResolved()) {
-          if (sibling.scope().lookup(ident) == null) {
-            notYetResolved.add(ident);
-          }
-        }
-
-        childClass.scope().notYetResolved(notYetResolved);
-      }
-    }
-
-    // Finally, try to extend each child class of the original file.
-    // If we can finish extending (find all parents), then add whatever is left unresolved to the
-    // global set of unresolved identifiers. If it is not totally resolved, add the intermediate
-    // result to the list of classes not fully extended
-    Set<Entity> notYetExtended = new HashSet<>();
-    for (Entity childClass : file.scope().notYetExtended()) {
-      if (loader.tryToExtend(childClass)) {
-        unresolved.addAll(childClass.scope().notYetResolved());
-        continue;
-      }
-
-      notYetExtended.add(childClass);
-    }
-
-    return new LoadResult(unresolved, notYetExtended);
   }
 }
--- core/src/main/java/com/nikodoko/javaimports/fixer/Loader.java
+++ core/src/main/java/com/nikodoko/javaimports/fixer/Loader.java
@@ -1,20 +1,110 @@
 package com.nikodoko.javaimports.fixer;
 
 import com.nikodoko.javaimports.parser.Entity;
+import com.nikodoko.javaimports.parser.Import;
 import com.nikodoko.javaimports.parser.ParsedFile;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
 class Loader {
   private Set<ParsedFile> siblings = new HashSet<>();
+  private Map<String, Import> candidates = new HashMap<>();
+  private ParsedFile file;
+
+  private Loader(ParsedFile file) {
+    this.file = file;
+  }
+
+  static Loader of(ParsedFile file) {
+    return new Loader(file);
+  }
 
   void addSiblings(Set<ParsedFile> siblings) {
     this.siblings = siblings;
   }
 
+  Map<String, Import> candidates() {
+    return candidates;
+  }
+
+  // Try to compute what identifiers are still unresolved, and what child classes are still not
+  // extended, using whatever information is available to the Fixer at the time.
+  LoadResult load() {
+    // Try to resolve with the main file imports.
+    // FIXME: this does not do anything about the situation where we import a class that we extend
+    // in the file. The problem is that we would need informations on the methods provided by said
+    // class so that we can decide which identifiers are still unresoled.
+    // We should probably have shortcut here that directly goes to find that package? But we most
+    // likely need environment information for this...
+    Set<String> unresolved = new HashSet<>();
+    for (String ident : file.scope().notYetResolved()) {
+      if (file.imports().get(ident) == null) {
+        unresolved.add(ident);
+      }
+    }
+
+    for (Entity childClass : file.scope().notYetExtended()) {
+      Set<String> notYetResolved = new HashSet<>();
+      for (String ident : childClass.scope().notYetResolved()) {
+        if (file.imports().get(ident) == null) {
+          notYetResolved.add(ident);
+        }
+      }
+
+      childClass.scope().notYetResolved(notYetResolved);
+    }
+
+    // Go over all identifiers in siblings and resolve what we can.
+    // XXX: We also try to resolve not yet extended classes, as there are two possibilities:
+    //  - that identifier is accessible in the package, but not defined in the parent class, so we
+    //  need to resolve it there
+    //  - that identifier is accessible in the package and shadowed by the parent class, but it does
+    //  not change the fact that it can be resolved anyway.
+    for (ParsedFile sibling : siblings) {
+      // Add all the imports in that file to the list of potential candidates
+      candidates.putAll(sibling.imports());
+
+      for (Iterator<String> i = unresolved.iterator(); i.hasNext(); ) {
+        if (sibling.scope().lookup(i.next()) != null) {
+          i.remove();
+        }
+      }
+
+      for (Entity childClass : file.scope().notYetExtended()) {
+        Set<String> notYetResolved = new HashSet<>();
+        for (String ident : childClass.scope().notYetResolved()) {
+          if (sibling.scope().lookup(ident) == null) {
+            notYetResolved.add(ident);
+          }
+        }
+
+        childClass.scope().notYetResolved(notYetResolved);
+      }
+    }
+
+    // Finally, try to extend each child class of the original file.
+    // If we can finish extending (find all parents), then add whatever is left unresolved to the
+    // global set of unresolved identifiers. If it is not totally resolved, add the intermediate
+    // result to the list of classes not fully extended
+    Set<Entity> notYetExtended = new HashSet<>();
+    for (Entity childClass : file.scope().notYetExtended()) {
+      if (tryToExtend(childClass)) {
+        unresolved.addAll(childClass.scope().notYetResolved());
+        continue;
+      }
+
+      notYetExtended.add(childClass);
+    }
+
+    return new LoadResult(unresolved, notYetExtended);
+  }
+
   // Try to extend a child class as much as possible (extending its parent if the parent itself is a
   // child, etc).
-  boolean tryToExtend(Entity childClass) {
+  private boolean tryToExtend(Entity childClass) {
     while (childClass.isChildClass()) {
       if (!tryToExtendOnce(childClass)) {
         // We could not extend it using any of the siblings
