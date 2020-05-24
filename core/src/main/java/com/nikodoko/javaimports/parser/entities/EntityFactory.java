9aacb12775a6e6b56f6f36100b6717dfe668b2b5
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-05-24 17:10:18 +0900

    Refactor Entity Factory

--- core/src/main/java/com/nikodoko/javaimports/parser/entities/EntityFactory.java
+++ core/src/main/java/com/nikodoko/javaimports/parser/entities/EntityFactory.java
@@ -5,17 +5,18 @@ import org.openjdk.javax.lang.model.element.Modifier;
 import org.openjdk.source.tree.ModifiersTree;
 
 public class EntityFactory {
+  // EntityModifiers provides sane defaults (except for classes)
   private static class EntityModifiers {
     Visibility visibility = Visibility.NONE;
     boolean isStatic = false;
   }
 
   public static Entity createClass(String name, ModifiersTree modifiers) {
-    return create(Kind.CLASS, name, parseModifiers(modifiers, Kind.METHOD));
+    return create(Kind.CLASS, name, parseClassModifiers(modifiers));
   }
 
   public static Entity createMethod(String name, ModifiersTree modifiers) {
-    return create(Kind.METHOD, name, parseModifiers(modifiers, Kind.METHOD));
+    return create(Kind.METHOD, name, parseModifiers(modifiers));
   }
 
   public static Entity createTypeParameter(String name) {
@@ -23,38 +24,64 @@ public class EntityFactory {
   }
 
   public static Entity createVariable(String name, ModifiersTree modifiers) {
-    return create(Kind.VARIABLE, name, parseModifiers(modifiers, Kind.VARIABLE));
+    return create(Kind.VARIABLE, name, parseModifiers(modifiers));
   }
 
   public static Entity createBad() {
     return create(Kind.BAD, "", new EntityModifiers());
   }
 
-  private static EntityModifiers parseModifiers(ModifiersTree modifiersTree, Kind kind) {
+  private static EntityModifiers parseClassModifiers(ModifiersTree modifiersTree) {
     EntityModifiers modifiers = new EntityModifiers();
+    Set<Modifier> flags = modifiersTree.getFlags();
+
+    modifiers.visibility = getClassVisibility(flags);
+    modifiers.isStatic = getStatic(flags);
+    return modifiers;
+  }
+
+  private static Visibility getClassVisibility(Set<Modifier> flags) {
+    Visibility visibility = getVisibility(flags);
+
+    if (visibility == Visibility.NONE) {
+      // Default for classes
+      return Visibility.PACKAGE_PRIVATE;
+    }
+
+    return visibility;
+  }
+
+  private static EntityModifiers parseModifiers(ModifiersTree modifiersTree) {
+    EntityModifiers modifiers = new EntityModifiers();
+    Set<Modifier> flags = modifiersTree.getFlags();
+
+    modifiers.visibility = getVisibility(flags);
+    modifiers.isStatic = getStatic(flags);
+    return modifiers;
+  }
+
+  private static Visibility getVisibility(Set<Modifier> flags) {
     // XXX: this dictates an arbitrary precedence between public, private and protected in the
     // unlikely case where several of them are present in modifiers at the same time. But this
     // should be forbidden by the java language, so we don't want to bother too much anyway.
-    Set<Modifier> flags = modifiersTree.getFlags();
-
     if (flags.contains(Modifier.PROTECTED)) {
-      modifiers.visibility = Visibility.PROTECTED;
+      return Visibility.PROTECTED;
     }
 
     if (flags.contains(Modifier.PRIVATE)) {
-      modifiers.visibility = Visibility.PRIVATE;
+      return Visibility.PRIVATE;
     }
 
     if (flags.contains(Modifier.PUBLIC)) {
-      modifiers.visibility = Visibility.PUBLIC;
+      return Visibility.PUBLIC;
     }
 
-    if (kind == Kind.CLASS) {
-      modifiers.visibility = Visibility.PACKAGE_PRIVATE;
-    }
+    // Default for all but class
+    return Visibility.NONE;
+  }
 
-    modifiers.isStatic = flags.contains(Modifier.STATIC);
-    return modifiers;
+  private static boolean getStatic(Set<Modifier> flags) {
+    return flags.contains(Modifier.STATIC);
   }
 
   private static Entity create(Kind kind, String name, EntityModifiers modifiers) {
