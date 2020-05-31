f72215c3a92797cda782514cc6ddb187e529b15e
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-05-31 18:49:12 +0900

    More ClassHierarchy tests

--- core/src/test/java/com/nikodoko/javaimports/parser/internal/ClassHierarchiesTest.java
+++ core/src/test/java/com/nikodoko/javaimports/parser/internal/ClassHierarchiesTest.java
@@ -4,22 +4,48 @@ import static com.google.common.truth.Truth8.assertThat;
 
 import com.nikodoko.javaimports.parser.entities.ClassEntity;
 import com.nikodoko.javaimports.parser.entities.Visibility;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ClassHierarchiesTest {
-  @Test
-  void testClassIsFound() {
-    ClassEntity A = new ClassEntity(Visibility.PUBLIC, true, "A");
-    ClassEntity B = new ClassEntity(Visibility.PUBLIC, true, "B");
-    ClassEntity C = new ClassEntity(Visibility.PUBLIC, true, "C");
+  static ClassHierarchy root;
+  static ClassEntity A = new ClassEntity(Visibility.PUBLIC, true, "A");
+  static ClassEntity B = new ClassEntity(Visibility.PUBLIC, true, "B");
+  static ClassEntity C1 = new ClassEntity(Visibility.PUBLIC, true, "C1");
+  static ClassEntity C2 = new ClassEntity(Visibility.PUBLIC, true, "C2");
 
-    ClassHierarchy hierarchy = ClassHierarchies.root();
-    ClassHierarchy root = hierarchy;
+  // Create the following:
+  // root
+  // |
+  // - A
+  //   |
+  //   - B
+  //
+  // leaf
+  // |
+  // - C1
+  // - C2
+  @BeforeAll
+  static void setupHierarchy() {
+    root = ClassHierarchies.root();
+
+    ClassHierarchy hierarchy = root;
     hierarchy = hierarchy.moveTo(A);
     hierarchy = hierarchy.moveTo(B);
     hierarchy = hierarchy.moveToLeaf();
-    hierarchy = hierarchy.moveTo(C);
+    hierarchy = hierarchy.moveTo(C1);
+    hierarchy = hierarchy.moveUp();
+    hierarchy = hierarchy.moveTo(C2);
+  }
 
+  @Test
+  void testFound() {
     assertThat(root.find(ClassSelectors.of("A"))).hasValue(A);
+    assertThat(root.find(ClassSelectors.of("A", "B"))).hasValue(B);
+  }
+
+  @Test
+  void testNotFound() {
+    assertThat(root.find(ClassSelectors.of("Z"))).isEmpty();
   }
 }
