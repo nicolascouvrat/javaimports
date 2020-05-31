3dd61d66ce174714f68622f773d43fb1a2e3a1a5
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-05-31 19:52:55 +0900

    Add method to create ClassSelector from extends clause

--- core/src/main/java/com/nikodoko/javaimports/parser/internal/ClassSelectors.java
+++ core/src/main/java/com/nikodoko/javaimports/parser/internal/ClassSelectors.java
@@ -1,18 +1,64 @@
 package com.nikodoko.javaimports.parser.internal;
 
 import java.util.Optional;
+import org.openjdk.tools.javac.tree.JCTree.JCExpression;
+import org.openjdk.tools.javac.tree.JCTree.JCFieldAccess;
+import org.openjdk.tools.javac.tree.JCTree.JCIdent;
+import org.openjdk.tools.javac.tree.JCTree.JCTypeApply;
 
 public class ClassSelectors {
   public static ClassSelector of(String first, String... others) {
     Selector head = new Selector(first);
     Selector tail = head;
     for (String other : others) {
-      Selector s = new Selector(other);
-      tail.next = s;
-      tail = s;
+      tail = updateTail(tail, other);
     }
 
     return head;
+  }
+
+  private static Selector updateTail(Selector previous, String next) {
+    Selector newTail = new Selector(next);
+    previous.next = newTail;
+    return newTail;
+  }
+
+  public static ClassSelector of(JCExpression extendsClause) {
+    JCExpression selector = extendsClause;
+    Selector head = null;
+    while (!(selector instanceof JCIdent)) {
+      if (selector instanceof JCTypeApply) {
+        selector = getGenericType(selector);
+        continue;
+      }
+
+      head = updateHead(head, getFieldIdentifier(selector));
+      selector = getFieldExpression(selector);
+    }
+
+    return updateHead(head, getIdentifierName(selector));
+  }
+
+  private static Selector updateHead(Selector previous, String next) {
+    Selector newHead = new Selector(next);
+    newHead.next = previous;
+    return newHead;
+  }
+
+  private static JCExpression getGenericType(JCExpression parametrized) {
+    return (JCExpression) ((JCTypeApply) parametrized).getType();
+  }
+
+  private static String getFieldIdentifier(JCExpression field) {
+    return ((JCFieldAccess) field).getIdentifier().toString();
+  }
+
+  private static JCExpression getFieldExpression(JCExpression field) {
+    return ((JCFieldAccess) field).getExpression();
+  }
+
+  private static String getIdentifierName(JCExpression identifier) {
+    return ((JCIdent) identifier).getName().toString();
   }
 
   static class Selector implements ClassSelector {
