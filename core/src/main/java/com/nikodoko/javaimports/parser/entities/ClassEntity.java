1d13e6ac602a78ccef52f99aedc93f9dd5a5b18c
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-06-03 23:24:25 +0900

    Cleanup ClassEntity

--- core/src/main/java/com/nikodoko/javaimports/parser/entities/ClassEntity.java
+++ core/src/main/java/com/nikodoko/javaimports/parser/entities/ClassEntity.java
@@ -3,7 +3,6 @@ package com.nikodoko.javaimports.parser.entities;
 import com.google.common.base.MoreObjects;
 import com.nikodoko.javaimports.parser.internal.ClassSelector;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import javax.annotation.Nullable;
@@ -13,8 +12,6 @@ public class ClassEntity {
   private final String name;
   private final boolean isStatic;
   @Nullable private final ClassSelector superclass;
-  // FIXME: remove me
-  private List<String> parentPath;
   private Set<String> members = new HashSet<>();
 
   public ClassEntity(Visibility visibility, boolean isStatic, String name) {
@@ -33,10 +30,6 @@ public class ClassEntity {
     return name;
   }
 
-  public Kind kind() {
-    return Kind.CLASS;
-  }
-
   public Set<String> members() {
     return members;
   }
@@ -50,39 +43,14 @@ public class ClassEntity {
     return Optional.ofNullable(superclass);
   }
 
-  // FIXME: remove this
-  @Nullable
-  public List<String> parentPath() {
-    return parentPath;
-  }
-
-  // FIXME: remove this
-  /** Set the extended class of this {@code Entity} */
-  public ClassEntity parentPath(List<String> path) {
-    this.parentPath = path;
-    return this;
-  }
-
-  /** Whether this {@code Entity} is extending anything */
-  public boolean isChildClass() {
-    return parentPath != null;
-  }
-
-  public ClassEntity clone() {
-    ClassEntity clone = new ClassEntity(visibility, isStatic, name);
-    clone.parentPath = parentPath;
-    clone.members = members;
-    return clone;
-  }
-
   /** Debugging support. */
   public String toString() {
     return MoreObjects.toStringHelper(this)
         .add("name", name)
         .add("visibility", visibility)
         .add("isStatic", isStatic)
-        .add("parentPath", parentPath)
         .add("members", members)
+        .add("superclass", superclass)
         .toString();
   }
 }
