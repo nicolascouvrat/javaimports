741cc286765c5b7c79db526018b072c2a0ebab31
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-06-15 19:01:49 +0900

    Implement case where there is only one candidate

--- core/src/main/java/com/nikodoko/javaimports/stdlib/BasicStdlibProvider.java
+++ core/src/main/java/com/nikodoko/javaimports/stdlib/BasicStdlibProvider.java
@@ -8,12 +8,26 @@ import java.util.Map;
 public class BasicStdlibProvider implements StdlibProvider {
   private Stdlib stdlib;
 
-  BasicStdlibProvider(Stdlib stlib) {
+  BasicStdlibProvider(Stdlib stdlib) {
     this.stdlib = stdlib;
   }
 
   @Override
   public Map<String, Import> find(Iterable<String> identifiers) {
-    return new HashMap<>();
+    Map<String, Import> found = new HashMap<>();
+    for (String identifier : identifiers) {
+      if (!stdlib.getClasses().containsKey(identifier)) {
+        continue;
+      }
+
+      if (stdlib.getClasses().get(identifier).length == 1) {
+        found.put(identifier, stdlib.getClasses().get(identifier)[0]);
+        continue;
+      }
+
+      throw new RuntimeException("not implemented");
+    }
+
+    return found;
   }
 }
