8a48709bf1643f3d98169cc303cfd5dbef4bf6b5
Nicolas Couvrat <nicolas.couvrat@sony.com>
2020-07-07 20:19:43 +0900

    Remove unused module

--- utilities/src/main/java/com/nikodoko/packagetest/exporters/Kind.java
+++
@@ -1,7 +0,0 @@
-package com.nikodoko.packagetest.exporters;
-
-/** The different types of {@link Exporter} available. */
-public enum Kind {
-  /** An exporter that generates a typical Maven (multi-module) project structure. */
-  MAVEN;
-}
