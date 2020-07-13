149d940c5895df437cfd164fb1a022891bbed77c
Nicolas Couvrat <nicolas.couvrat@mines-paristech.fr>
2020-07-13 16:29:52 +0900

    Test success and failure behavior for scanPackageFiles

--- core/src/test/java/com/nikodoko/javaimports/resolver/maven/MavenProjectScannerTest.java
+++ core/src/test/java/com/nikodoko/javaimports/resolver/maven/MavenProjectScannerTest.java
@@ -1,13 +1,9 @@
 package com.nikodoko.javaimports.resolver.maven;
 
-import static com.google.common.truth.Truth8.assertThat;
+import static com.google.common.truth.Truth.assertThat;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.nikodoko.javaimports.Options;
-import com.nikodoko.javaimports.parser.Import;
-import com.nikodoko.javaimports.resolver.Resolver;
-import com.nikodoko.javaimports.resolver.Resolvers;
 import com.nikodoko.packagetest.Export;
 import com.nikodoko.packagetest.Exported;
 import com.nikodoko.packagetest.Module;
@@ -16,7 +12,7 @@ import java.nio.file.Path;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
-public class MavenResolverTest {
+public class MavenProjectScannerTest {
   Exported project;
 
   @AfterEach
@@ -25,55 +21,72 @@ public class MavenResolverTest {
   }
 
   @Test
-  void testThatTopLevelClassesAreFound() throws Exception {
+  void testFindFilesOfSamePackageInDifferentFolder() throws Exception {
     Module module =
         new Module(
             "test.module",
             ImmutableMap.of(
                 "Main.java",
                 "package test.module; public class Main {}",
+                // same package despite being in another folder
                 "second/Second.java",
-                "package test.module.second; public class Second {}"));
+                "package test.module; public class Second {}"));
     project = Export.of(Kind.MAVEN, ImmutableList.of(module));
-    Path target = project.file(module.name(), "Main.java").get();
 
-    Resolver resolver = Resolvers.basedOnEnvironment(target, Options.defaults());
-    assertThat(resolver.find("Second")).hasValue(new Import("Second", "test.module.second", false));
+    MavenProjectScanner scanner = MavenProjectScanner.withRoot(project.root());
+    MavenProjectScanner.Result got = scanner.scanFilesInPackage("test.module");
+
+    assertThat(got.files).hasSize(2);
+    assertThat(got.errors).hasSize(0);
   }
 
   @Test
-  void testThatFileBeingResolvedIsNotFound() throws Exception {
+  void testScannerIgnoresExcludedFiles() throws Exception {
     Module module =
         new Module(
             "test.module",
             ImmutableMap.of(
                 "Main.java",
                 "package test.module; public class Main {}",
+                // same package despite being in another folder
                 "second/Second.java",
                 "package test.module.second; public class Second {}"));
     project = Export.of(Kind.MAVEN, ImmutableList.of(module));
     Path target = project.file(module.name(), "Main.java").get();
 
-    Resolver resolver = Resolvers.basedOnEnvironment(target, Options.defaults());
-    assertThat(resolver.find("Main")).isEmpty();
+    MavenProjectScanner scanner = MavenProjectScanner.withRoot(project.root()).excluding(target);
+    MavenProjectScanner.Result got = scanner.scanFilesInPackage("test.module");
+
+    assertThat(got.files).hasSize(0);
+    assertThat(got.errors).hasSize(0);
+
+    // TODO: test scanAll
   }
 
   @Test
-  void testThatFindPicksTheClosestImport() throws Exception {
+  void testScannerDoesItsBestDespiteErrors() throws Exception {
     Module module =
         new Module(
             "test.module",
             ImmutableMap.of(
                 "Main.java",
                 "package test.module; public class Main {}",
-                "other/second/Second.java",
-                "package test.module.other.second; public class Second {}",
+                "Other.java",
+                "package test.module; public class Other {}",
+                "Invalid.java",
+                "this is not valid java code",
+                // same package despite being in another folder
                 "second/Second.java",
                 "package test.module.second; public class Second {}"));
     project = Export.of(Kind.MAVEN, ImmutableList.of(module));
     Path target = project.file(module.name(), "Main.java").get();
 
-    Resolver resolver = Resolvers.basedOnEnvironment(target, Options.defaults());
-    assertThat(resolver.find("Second")).hasValue(new Import("Second", "test.module.second", false));
+    MavenProjectScanner scanner = MavenProjectScanner.withRoot(project.root()).excluding(target);
+    MavenProjectScanner.Result got = scanner.scanFilesInPackage("test.module");
+
+    assertThat(got.files).hasSize(1);
+    assertThat(got.errors).hasSize(1);
+
+    // TODO: test scanAll
   }
 }
