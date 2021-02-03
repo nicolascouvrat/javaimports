370ddf2f3cd8fb8bb4bd13a76d85479aea05fb28
Nicolas Couvrat <nicolas.couvrat@datadoghq.com>
2021-02-03 19:26:05 +0100

    Factor pom loading out of DependencyFinder

--- core/src/main/java/com/nikodoko/javaimports/environment/maven/MavenDependencyFinder.java
+++ core/src/main/java/com/nikodoko/javaimports/environment/maven/MavenDependencyFinder.java
@@ -1,18 +1,14 @@
 package com.nikodoko.javaimports.environment.maven;
 
 import com.google.common.base.MoreObjects;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import org.apache.maven.model.Dependency;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.DefaultModelReader;
 
 /** Finds all dependencies in a Maven project by parsing POM files. */
+// TODO: iterate over parent poms if the dependencies do not have a set version
+// TODO: handle dependency management
 class MavenDependencyFinder {
   static final class Result {
     final List<MavenDependency> dependencies = new ArrayList<>();
@@ -26,52 +22,14 @@ class MavenDependencyFinder {
     }
   }
 
+  private static final Path POM = Paths.get("pom.xml");
   private Result result = new Result();
-  private boolean allFound = false;
 
   Result findAll(Path moduleRoot) {
-    Path target = moduleRoot;
-    while (!allFound && hasPom(target)) {
-      tryToScanPom(target);
-      target = target.getParent();
-    }
+    var loaded = new MavenPomLoader().load(moduleRoot.resolve(POM));
+    result.dependencies.addAll(loaded.dependencies);
+    result.errors.addAll(loaded.errors);
 
     return result;
-  }
-
-  private boolean hasPom(Path directory) {
-    Path pom = Paths.get(directory.toString(), "pom.xml");
-    return Files.exists(pom);
-  }
-
-  private void tryToScanPom(Path directory) {
-    try {
-      scanPom(directory);
-    } catch (IOException e) {
-      result.errors.add(
-          new MavenEnvironmentException(String.format("could not scan pom in %s", directory), e));
-    }
-  }
-
-  private void scanPom(Path directory) throws IOException {
-    File pom = Paths.get(directory.toString(), "pom.xml").toFile();
-    Model model = new DefaultModelReader().read(pom, null);
-    List<Dependency> deps = model.getDependencies();
-    scanDependencies(deps);
-  }
-
-  private void scanDependencies(List<Dependency> deps) {
-    boolean ok = true;
-    for (Dependency dep : deps) {
-      MavenDependency dependency =
-          new MavenDependency(dep.getGroupId(), dep.getArtifactId(), dep.getVersion());
-      result.dependencies.add(dependency);
-
-      if (!dependency.hasPlainVersion()) {
-        ok = false;
-      }
-    }
-
-    allFound = ok;
   }
 }
